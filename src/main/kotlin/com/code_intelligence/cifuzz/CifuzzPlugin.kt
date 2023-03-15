6fa8c2cdb1ffb0f28dd2bd006934e2a291ee8542
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-03-15 12:20:49 +0100

    Use 'ReportingExtension' and 'jacoco-report-aggregation'

--- src/main/kotlin/com/code_intelligence/cifuzz/CifuzzPlugin.kt
+++ src/main/kotlin/com/code_intelligence/cifuzz/CifuzzPlugin.kt
@@ -4,10 +4,20 @@ import com.code_intelligence.cifuzz.tasks.BuildDirectoryPrinter
 import com.code_intelligence.cifuzz.tasks.ClasspathPrinter
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.component.ProjectComponentIdentifier
+import org.gradle.api.artifacts.type.ArtifactTypeDefinition
+import org.gradle.api.attributes.Category
+import org.gradle.api.attributes.TestSuiteType
+import org.gradle.api.attributes.VerificationType
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.plugins.jvm.JvmTestSuite
+import org.gradle.api.provider.Provider
+import org.gradle.api.reporting.ReportingExtension
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.api.tasks.testing.Test
-import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
-import org.gradle.testing.jacoco.tasks.JacocoReport
+import org.gradle.testing.base.TestingExtension
+import org.gradle.testing.jacoco.plugins.JacocoCoverageReport
 
 abstract class CIFuzzPlugin : Plugin<Project> {
 
@@ -18,9 +28,12 @@ abstract class CIFuzzPlugin : Plugin<Project> {
     }
 
     private fun configureCIFuzz(project: Project) {
-        project.plugins.apply("jacoco")
+        project.plugins.apply("jacoco-report-aggregation")
 
-        val sourceSets = project.extensions.getByType(SourceSetContainer::class.java)
+        val objects = project.objects
+        val testing = project.extensions.getByType(TestingExtension::class.java)
+        val reporting = project.extensions.getByType(ReportingExtension::class.java)
+
         val fuzzTestProperty = project.providers.gradleProperty("cifuzz.fuzztest")
 
         registerPrintBuildDir(project)
@@ -30,30 +43,25 @@ abstract class CIFuzzPlugin : Plugin<Project> {
             configureAllTestTasks(project, fuzzTestProperty.get())
         }
 
-        // we register own tasks for the report to avoid side effects on existing user tasks
-        // when overwriting config values (like output path)
-
-        // we need to set the exec file output path explicitly to make sure we find
-        // the file in the `cifuzzReport` tasks
-        project.tasks.register("cifuzzReport", JacocoReport::class.java) { cifuzzReport ->
-            val allTestTasks = project.tasks.withType(Test::class.java)
-            cifuzzReport.dependsOn(allTestTasks)
-            cifuzzReport.executionData(allTestTasks.map { testTask ->
-                testTask.extensions.getByType(JacocoTaskExtension::class.java).destinationFile!!
-            })
-            cifuzzReport.classDirectories.from(project.files(sourceSets.getByName("main").output))
-            cifuzzReport.sourceDirectories.from(project.files(sourceSets.getByName("main").java.srcDirs))
-
-            cifuzzReport.reports { reports ->
-                val output = project.providers.gradleProperty("cifuzz.report.output").orElse(
-                    project.layout.buildDirectory.dir("reports/cifuzz").map { it.asFile.absolutePath }
+        // We register own tasks for the report to avoid side effects on existing user tasks when overwriting config
+        // values (like the output path).
+        reporting.reports.register("cifuzzReport", JacocoCoverageReport::class.java) { report ->
+            // project.tasks.register("cifuzzReport", JacocoReport::class.java) { cifuzzReport -> }
+            report.testType.set(TestSuiteType.UNIT_TEST)
+            report.reportTask.configure { cifuzzReport ->
+                val codeCoverageResults = project.configurations.getByName("aggregateCodeCoverageReportResults")
+                cifuzzReport.executionData.setFrom(
+                    testing.suites.withType(JvmTestSuite::class.java).map {
+                        artifactsForTestType(codeCoverageResults, objects, it.testType)
+                    }
                 )
-                val format = project.providers.gradleProperty("cifuzz.report.format").getOrElse("html")
 
-                reports.html.required.set(format != "jacocoxml")
-                reports.html.outputLocation.set(output.map { project.layout.projectDirectory.dir("$it/html") })
-                reports.xml.required.set(true)
-                reports.xml.outputLocation.set(output.map { project.layout.projectDirectory.file("$it/jacoco.xml") })
+                cifuzzReport.reports { reports ->
+                    // TODO Is the 'cifuzz.report.format' configuration needed?
+                    val format = project.providers.gradleProperty("cifuzz.report.format").getOrElse("html")
+                    reports.html.required.set(format != "jacocoxml")
+                    reports.xml.required.set(true)
+                }
             }
         }
     }
@@ -62,7 +70,11 @@ abstract class CIFuzzPlugin : Plugin<Project> {
         project.tasks.withType(Test::class.java).configureEach { testTask ->
             testTask.ignoreFailures = true
             testTask.jvmArgs("-Djazzer.hooks=false") // disable jazzer hooks as they are not needed for coverage runs
-            testTask.filter { filter -> filter.includeTestsMatching(fuzzTestFilter) }
+            // TODO filter by FuzzTest annotation
+            testTask.filter { filter ->
+                filter.includeTestsMatching(fuzzTestFilter)
+                filter.isFailOnNoMatchingTests = false
+            }
         }
     }
 
@@ -79,4 +91,28 @@ abstract class CIFuzzPlugin : Plugin<Project> {
             printClasspath.testRuntimeClasspath.from(sourceSets.getByName("test").runtimeClasspath)
         }
     }
+
+    private fun artifactsForTestType(codeCoverageResults: Configuration, objects: ObjectFactory, testType: Provider<String>) =
+        codeCoverageResults.incoming.artifactView { view ->
+            view.withVariantReselection()
+            view.componentFilter { id -> id is ProjectComponentIdentifier }
+            view.attributes { attributes ->
+                attributes.attribute(
+                    Category.CATEGORY_ATTRIBUTE,
+                    objects.named(Category::class.java, Category.VERIFICATION)
+                )
+                attributes.attributeProvider(
+                    TestSuiteType.TEST_SUITE_TYPE_ATTRIBUTE,
+                    testType.map { objects.named(TestSuiteType::class.java, it) }
+                )
+                attributes.attribute(
+                    VerificationType.VERIFICATION_TYPE_ATTRIBUTE,
+                    objects.named(VerificationType::class.java, VerificationType.JACOCO_RESULTS)
+                )
+                attributes.attribute(
+                    ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE,
+                    ArtifactTypeDefinition.BINARY_DATA_TYPE
+                )
+            }
+        }.files
 }
