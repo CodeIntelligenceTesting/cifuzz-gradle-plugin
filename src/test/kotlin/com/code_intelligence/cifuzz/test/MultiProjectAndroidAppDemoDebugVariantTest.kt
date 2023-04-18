0e6299f30550697cb4a50cb896eb23046499b969
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-04-18 09:42:11 +0200

    Extend Android test coverage: Selection of different variants

---
+++ src/test/kotlin/com/code_intelligence/cifuzz/test/MultiProjectAndroidAppDemoDebugVariantTest.kt
@@ -0,0 +1,33 @@
+package com.code_intelligence.cifuzz.test
+
+import org.junit.jupiter.api.BeforeEach
+import java.io.File
+
+
+class MultiProjectAndroidAppDemoDebugVariantTest : MultiProjectAndroidTest() {
+
+    override fun testedAndroidVariant() = "demoDebug"
+
+    @BeforeEach
+    fun adjustSample() {
+        File(projectDir, "app/build.gradle.kts").appendText("""
+            cifuzz.androidVariant.set("demoDebug")    
+            
+            android {
+                flavorDimensions += "version"
+                productFlavors {
+                    create("demo") {
+                        dimension = "version"
+                        applicationIdSuffix = ".demo"
+                        versionNameSuffix = "-demo"
+                    }
+                    create("full") {
+                        dimension = "version"
+                        applicationIdSuffix = ".full"
+                        versionNameSuffix = "-full"
+                    }
+                }
+            }
+        """.trimIndent())
+    }
+}
---
+++ src/test/kotlin/com/code_intelligence/cifuzz/test/MultiProjectAndroidAppReleaseVariantTest.kt
@@ -0,0 +1,7 @@
+package com.code_intelligence.cifuzz.test
+
+
+class MultiProjectAndroidAppReleaseVariantTest : MultiProjectAndroidTest() {
+
+    override fun testedAndroidVariant() = "release"
+}
